9e0f4ca9f2ad5294aea3a18030d3dc7b6f0c55c5
vivekv95 <vivekv210@gmail.com>
2019-10-23 01:19:39 -0500

    Create AuthModule and Provider for AuthApi instance

---
+++ DaggerPractice/app/src/main/java/com/example/daggerpractice/di/auth/AuthModule.kt
@@ -0,0 +1,19 @@
+package com.example.daggerpractice.di.auth
+
+import com.example.daggerpractice.network.auth.AuthApi
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+
+@Module
+class AuthModule {
+
+    @Module
+    companion object {
+
+        @Provides @JvmStatic
+        fun provideAuthApi(retrofit: Retrofit): AuthApi {
+            return retrofit.create(AuthApi::class.java)
+        }
+    }
+}
--- DaggerPractice/app/src/main/java/com/example/daggerpractice/network/auth/AuthApi.kt
+++ DaggerPractice/app/src/main/java/com/example/daggerpractice/network/auth/AuthApi.kt
@@ -1,7 +1,10 @@
 package com.example.daggerpractice.network.auth
 
+import okhttp3.Response
+import okhttp3.ResponseBody
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface AuthApi {
-   
+    fun getFakeStuff(): Call<ResponseBody>
 }
