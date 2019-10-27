53743ec47b6682a993ddf0d98bb7142aaa6872a3
vivekv95 <vivekv210@gmail.com>
2019-10-27 01:20:57 -0500

    Add navigation with backstack management

--- DaggerPractice/app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.kt
+++ DaggerPractice/app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.kt
@@ -9,6 +9,9 @@ import android.view.MenuItem
 import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.daggerpractice.R
 import com.example.daggerpractice.ui.auth.AuthActivity
 import com.example.daggerpractice.model.AuthResource
@@ -24,16 +27,32 @@ class MainActivity : DaggerAppCompatActivity(), NavigationView.OnNavigationItemS
     override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
         when (menuItem.itemId) {
             R.id.nav_profile -> {
-
+                val navOptions = NavOptions.Builder()
+                    .setPopUpTo(R.id.main, true)
+                    .build()
+                Navigation.findNavController(
+                    this,
+                    R.id.nav_host_fragment_container
+                ).navigate(R.id.profileScreen, null, navOptions)
             }
-            R.id.nav_posts -> {
+            R.id.nav_posts ->
+                if (isValidDestination(R.id.postsScreen))
+                    Navigation.findNavController(
+                        this,
+                        R.id.nav_host_fragment_container
+                    ).navigate(R.id.postsScreen)
 
-            }
         }
         menuItem.isChecked = true
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
+
+    private fun isValidDestination(destination: Int): Boolean =
+        destination != Navigation
+            .findNavController(this, R.id.nav_host_fragment_container)
+            .currentDestination
+            ?.id
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -46,7 +65,7 @@ class MainActivity : DaggerAppCompatActivity(), NavigationView.OnNavigationItemS
 
         viewModel = ViewModelProviders.of(this, viewModelFactory)[MainViewModel::class.java]
         subscribeObservers()
-        testFragment()
+        init()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -55,21 +74,22 @@ class MainActivity : DaggerAppCompatActivity(), NavigationView.OnNavigationItemS
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.logout -> {
                 viewModel.logOut()
                 return true
             }
+            android.R.id.home -> {
+                return if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+                    drawer_layout.closeDrawer(GravityCompat.START)
+                    true
+                } else
+                    false
+
+            }
         }
         return super.onOptionsItemSelected(item)
     }
-
-    fun testFragment() =
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.main_container, PostsFragment())
-            .commit()
-
 
     private fun subscribeObservers() =
         viewModel.observeAuthState().observe(this, Observer { authResource ->
@@ -99,5 +119,20 @@ class MainActivity : DaggerAppCompatActivity(), NavigationView.OnNavigationItemS
             startActivity(this)
             finish()
         }
+
+    private fun init() {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI
+            .navigateUp(
+                Navigation.findNavController(this, R.id.nav_host_fragment_container),
+                drawer_layout
+            )
+    }
 
 }
